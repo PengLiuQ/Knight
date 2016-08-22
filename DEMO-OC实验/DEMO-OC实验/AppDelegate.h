61f0652a2b9c87ddab9ce80581726e06f38ed695
tens04 <tens04@lillys-mac.local>
2016-08-22 15:50:13 +0800

    GILIGILIAI

---
+++ DEMO-OC实验/DEMO-OC实验/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DEMO-OC实验
+//
+//  Created by tens04 on 16/8/22.
+//  Copyright © 2016年 xuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DEMO-OC实验/DEMO-OC实验/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DEMO-OC实验
+//
+//  Created by tens04 on 16/8/22.
+//  Copyright © 2016年 xuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
